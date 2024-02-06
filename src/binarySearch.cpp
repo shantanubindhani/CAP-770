ef5d8578178bc203ab63cb70a8cc5766f93862f0
Shantanu Bindhani <48177913+shantanubindhani@users.noreply.github.com>
2024-02-06 21:06:47 +0530

    Update binarySearch.cpp

--- src/binarySearch.cpp
+++ src/binarySearch.cpp
@@ -7,19 +7,18 @@ int main(){
     int n; cin>>n;
     int *nums = new int[n];
     for(int i = 0; i<n; ++i) cin>>nums[i];
-    for(int i = 0; i<n; ++i) cout<<nums[i]<<" "; cout<<endl;
+    //for(int i = 0; i<n; ++i) cout<<nums[i]<<" "; cout<<endl; // to view the input
     int target; cin>>target;
     int res = binarySearch(nums, target, 0, n-1);
     if(res == -1) cout<<"Not Found.";
     else cout<<"Found at : "<<res<<endl;
 
     return 0;
-
 }
 
 int binarySearch(int arr[],int target, int head, int tail){
     int mid = head + (tail-head)/2;
-    // cout<<head<<" "<<mid<<" "<<tail<<endl;
+    // cout<<head<<" "<<mid<<" "<<tail<<endl; // to debug
     if(target < arr[mid]) return binarySearch(arr, target, head, mid-1);
     if(target > arr[mid]) return binarySearch(arr, target, mid+1, tail);
     if(arr[mid] == target) return mid;
